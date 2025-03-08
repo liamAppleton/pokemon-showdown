8942144a013f313582596bff2c13c7f3f44a1bf2
Liam Appleton <liamappleton97@outlook.com>
2025-03-08 14:07:36 +0000

    Potion class use method passing all tests, Potion class finished

--- __tests__/potion-class.test.js
+++ __tests__/potion-class.test.js
@@ -1,4 +1,4 @@
-const { Potion } = require('../classes');
+const { NormalPokemon, Potion } = require('../classes');
 
 describe('Potion Class', () => {
   describe('properties', () => {
@@ -13,8 +13,26 @@ describe('Potion Class', () => {
   });
 
   describe('methods', () => {
+    let lopunny, potion1;
+    beforeEach(() => {
+      lopunny = new NormalPokemon('Lopunny', 'Tackle', 100, 20);
+      potion1 = new Potion('Hyper Potion', 50);
+    });
+
     describe('use()', () => {
-      test('should');
+      test('instances should have a use method', () => {
+        expect(typeof potion1.use).toBe('function');
+      });
+      test("should increase a pokemon's hitPoints by restores", () => {
+        lopunny.takeDamage(60);
+        potion1.use(lopunny);
+        expect(lopunny.hitPoints).toBe(90);
+      });
+      test('if restores greater than maxHitPoints, hitPoints should be equal to maxHitPoints', () => {
+        lopunny.takeDamage(10);
+        potion1.use(lopunny);
+        expect(lopunny.hitPoints).toBe(100);
+      });
     });
   });
 });
--- classes/potion-class.js
+++ classes/potion-class.js
@@ -3,6 +3,15 @@ class Potion {
     this.name = name;
     this.restores = restores;
   }
+
+  use(pokemon) {
+    const hitPointsAfterRestore = (pokemon.hitPoints += this.restores);
+    if (hitPointsAfterRestore > pokemon.maxHitPoints) {
+      pokemon.hitPoints = pokemon.maxHitPoints;
+    } else {
+      pokemon.hitPoints = hitPointsAfterRestore;
+    }
+  }
 }
 
 module.exports = Potion;
