b13ba3943d7a927d9ce4488e05917584e07ac091
Liam Appleton <liamappleton97@outlook.com>
2025-03-08 22:12:33 +0000

    starting inquirer prompts

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+
+const initialSelection = async () => {
+  const selectionQuestion = {
+    type: 'list',
+    name: 'pokemon',
+    message: 'Select a pokemon',
+    choices: [1, 2, 3],
+    loop: true,
+  };
+  return inquirer.prompt(selectionQuestion);
+};
+
+const main = async () => {
+  let i = 0;
+  while (i < 6) {
+    await initialSelection();
+    i++;
+  }
+};
+
+main();
