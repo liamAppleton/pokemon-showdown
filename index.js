5cea031a3530519ae534fcccce8efd346336ce55
Liam Appleton <liamappleton97@outlook.com>
2025-03-09 10:05:29 +0000

    creating inquirer prompts file

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const inquirer = require('inquirer');
-const { pokemon } = require('./class-instances');
-const { pokemonNameList } = require('./game-data');
-const { deletePokemon, pokemonLookup } = require('./utils');
-
-const playerSelections = [];
-
-const initialSelection = async () => {
-  const selectionQuestion = {
-    type: 'list',
-    name: 'selectedPokemon',
-    message: 'Select a pokemon: ',
-    choices: pokemonNameList,
-    loop: true,
-  };
-  return inquirer.prompt(selectionQuestion).then(({ selectedPokemon }) => {
-    const index = pokemonNameList.indexOf(selectedPokemon);
-    pokemonNameList.splice(index, 1); // remove from name list
-
-    const emojiRemovedName = selectedPokemon.match(/[a-z]+/i)[0];
-    playerSelections.push(pokemonLookup(pokemon, emojiRemovedName));
-    console.log(playerSelections);
-
-    deletePokemon(pokemon, emojiRemovedName); // remove from pokemon object
-  });
-};
+const { initialSelection } = require('./inquirer-prompts');
 
 const main = async () => {
   let i = 0;
---
+++ inquirer-prompts.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+const { pokemon } = require('./class-instances');
+const { pokemonNameList } = require('./game-data');
+const { deletePokemon, pokemonLookup } = require('./utils');
+
+const playerSelections = [];
+
+const initialSelection = async () => {
+  const selectionQuestion = {
+    type: 'list',
+    name: 'selectedPokemon',
+    message: 'Select a pokemon: ',
+    choices: pokemonNameList,
+    loop: true,
+  };
+  return inquirer.prompt(selectionQuestion).then(({ selectedPokemon }) => {
+    const index = pokemonNameList.indexOf(selectedPokemon);
+    pokemonNameList.splice(index, 1); // remove from name list
+
+    const emojiRemovedName = selectedPokemon.match(/[a-z]+/i)[0];
+    playerSelections.push(pokemonLookup(pokemon, emojiRemovedName));
+
+    deletePokemon(pokemon, emojiRemovedName); // remove from pokemon object
+  });
+};
+
+module.exports = { initialSelection, playerSelections };
